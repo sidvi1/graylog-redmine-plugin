35a13db6c18f60fb6d0d2578ad4f155396fd51c3
sidvi <m00n.post@gmail.com>
2016-08-08 18:09:50 +0300

    Fix 2 checkstyle errors

--- src/main/java/ru/sidvi/graylog/IssueDTO.java
+++ src/main/java/ru/sidvi/graylog/IssueDTO.java
@@ -77,8 +77,12 @@ public class IssueDTO {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         IssueDTO issueDTO = (IssueDTO) o;
         return Objects.equal(title, issueDTO.title) &&
                 Objects.equal(description, issueDTO.description) &&
